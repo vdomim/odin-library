071197a9bbb660015b1c0f3312fc44655a6a3b2a
Victor Dominguez <vdomim@gmail.com>
2023-11-02 01:21:07 +0100

    Adding book feature

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 const dialog = document.querySelector('dialog')
 const addBtn = document.querySelector('.addBook')
-const closeButton = document.querySelector("dialog button")
+const submitBtn = document.querySelector('.submit')
+const closeButton = document.querySelector('dialog button')
+const cardContainter = document.querySelector('.card-container')
 
-console.log(dialog);
-console.log(addBtn);
+console.log(submitBtn)
+console.log(addBtn)
 
-const myLibrary = [];
+const myLibrary = []
 
-function Book(title, author, pages) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.readed = false;
+function Book(title, author, pages, readed) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    console.log(readed)
 
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${readed ? 'readed' : 'not readed yet'}`;
+    this.readed = readed
+    this.info = function () {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${
+            readed ? 'readed' : 'not readed yet'
+        }`
     }
 }
 
@@ -22,15 +27,84 @@ function addBookToLibrary(book) {
     myLibrary.push(book)
 }
 
-const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295 );
-addBookToLibrary(book1);
+const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295)
+const book2 = new Book('The Hobbit 2', 'J.R.R. Tolkien', 300)
+addBookToLibrary(book1)
+addBookToLibrary(book2)
 
+paintBooks()
+
+function paintNewBook(book) {
+    cardContainter.appendChild(createNewCard(book))
+}
+
+function paintBooks() {
+    myLibrary.forEach((book) => {
+        cardContainter.appendChild(createNewCard(book))
+    })
+}
+
+function createNewCard(book) {
+    const newCard = document.createElement('div')
+    newCard.className = 'card'
+
+    const newCardContent = createNewCardContent(book)
+    const newCardActions = createNewCardActions()
+    newCard.appendChild(newCardContent)
+    newCard.appendChild(newCardActions)
+    return newCard
+}
+
+function createNewCardActions() {
+    const newCardActions = document.createElement('div')
+    newCardActions.className = 'card-actions'
+    const newButtonEliminar = document.createElement('button')
+    const btnEliminarText = document.createTextNode('Eliminar')
+    newButtonEliminar.appendChild(btnEliminarText)
+    const newButtonReaded = document.createElement('button')
+    const btnReadedText = document.createTextNode('Cambiar estado')
+    newButtonReaded.appendChild(btnReadedText)
+    newCardActions.append(newButtonEliminar, newButtonReaded)
+    return newCardActions
+}
+
+function createNewCardContent(book) {
+    const newCardContent = document.createElement('div')
+    newCardContent.className = 'card-content'
+    const title = createNewElement(book.title)
+    const author = createNewElement(book.author)
+    const pages = createNewElement(book.pages + ' pages')
+    const readed = createNewElement(book.readed ? 'leido' : 'sin leer')
+    newCardContent.append(title, author, pages, readed)
+    return newCardContent
+}
+function createNewElement(input) {
+    const element = document.createElement('p')
+    const elementText = document.createTextNode(input)
+    element.appendChild(elementText)
+    return element
+}
 
 addBtn.addEventListener('click', () => {
     dialog.showModal()
 })
 
 // "Close" button closes the dialog
-closeButton.addEventListener("click", () => {
-    dialog.close();
-});
+closeButton.addEventListener('click', () => {
+    dialog.close()
+})
+
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    console.log('hika')
+
+    let title = document.querySelector('#title').value
+    let author = document.querySelector('#author').value
+    let pages = document.querySelector('#numPages').value
+    let readed = Boolean(document.querySelector('#readed').checked)
+    console.log(readed)
+
+    const newBook = new Book(title, author, pages, readed)
+    addBookToLibrary(newBook)
+    paintNewBook(newBook)
+})
