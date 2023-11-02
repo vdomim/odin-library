02340422fd85c74599a4352790dd8c42b556b3d2
Victor Dominguez <vdomim@gmail.com>
2023-11-02 18:42:23 +0100

    Adding toggle functionality

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const addBtn = document.querySelector('.addBook')
 const submitBtn = document.querySelector('.submit')
 const closeButton = document.querySelector('dialog button')
 const cardContainter = document.querySelector('.card-container')
+const removeBtn = document.querySelector('.remove-book')
+const readedBtn = document.querySelector('.readed img')
 
 const myLibrary = []
 
@@ -24,7 +26,7 @@ function addBookToLibrary(book) {
 }
 
 const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
-const book2 = new Book('The Hobbit 2', 'J.R.R. Tolkien', 300, false)
+const book2 = new Book('The Hobbit 2', 'J.R.R. Tolkien', 300, true)
 addBookToLibrary(book1)
 addBookToLibrary(book2)
 
@@ -36,7 +38,7 @@ function paintNewBook(book) {
 
 function paintBooks() {
     myLibrary.forEach((book) => {
-        cardContainter.appendChild(createNewCard(book))
+        paintNewBook(book)
     })
 }
 
@@ -45,21 +47,37 @@ function createNewCard(book) {
     newCard.className = 'card'
 
     const newCardContent = createNewCardContent(book)
-    const newCardActions = createNewCardActions()
+    const newCardActions = createNewCardActions(book)
     newCard.appendChild(newCardContent)
     newCard.appendChild(newCardActions)
     return newCard
 }
 
-function createNewCardActions() {
+function createNewCardActions(book) {
     const newCardActions = document.createElement('div')
     newCardActions.className = 'card-actions'
     const newButtonEliminar = document.createElement('button')
-    const btnEliminarText = document.createTextNode('Eliminar')
-    newButtonEliminar.appendChild(btnEliminarText)
+    const newImgEliminar = document.createElement('img')
+    newImgEliminar.className = "remove-book"
+    newImgEliminar.src = './imgs/book-remove-outline.svg'
+    newButtonEliminar.appendChild(newImgEliminar)
+
     const newButtonReaded = document.createElement('button')
-    const btnReadedText = document.createTextNode('Cambiar estado')
-    newButtonReaded.appendChild(btnReadedText)
+    newButtonReaded.className = 'readed'
+    const newImgReaded = document.createElement('img')
+
+    if(book.readed) {
+        newImgReaded.className = 'readed'
+        newImgReaded.src = './imgs/bookmark-check-outline.svg'
+    } else {
+        newImgReaded.className = 'not-readed'
+        newImgReaded.src = './imgs/bookmark-outline.svg'
+    }
+    newImgReaded.addEventListener('click', (event) => {
+        toggleReaded(event)
+    })
+    newButtonReaded.appendChild(newImgReaded)
+
     newCardActions.append(newButtonEliminar, newButtonReaded)
     return newCardActions
 }
@@ -89,6 +107,17 @@ addBtn.addEventListener('click', () => {
 closeButton.addEventListener('click', () => {
     dialog.close()
 })
+
+function toggleReaded (event) {
+    if(event.target.className==='readed'){
+        event.target.className = 'not-readed'
+        event.target.src = './imgs/bookmark-outline.svg'
+    }else{
+        event.target.className = 'readed'
+        event.target.src = './imgs/bookmark-check-outline.svg'
+    }
+}
+
 
 submitBtn.addEventListener('click', (event) => {
     event.preventDefault()
